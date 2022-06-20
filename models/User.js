8ed68d67f6cc0a371a8f3421992928d26624a959
ammyy9908 <sb78639@gmail.com>
2022-06-20 07:53:15 +0530

    cart added to user module

--- models/User.js
+++ models/User.js
@@ -13,6 +13,10 @@ const user_schema = new Schema({
     type: String,
     required: true,
   },
+  cart: {
+    type: Array,
+    default: [],
+  },
 });
 
 const User = model("User", user_schema);
